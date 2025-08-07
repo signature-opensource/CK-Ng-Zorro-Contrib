30acef9a0dd1ff6f682e33ca2578ac9f14dc1b3c
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-08-07 13:27:09 +0200

    Removed duplicated imports.
    These are already provided by CK.Ng.Zorro.

--- CK.Ng.Zorro.BackOffice/BackOfficePackage.cs
+++ CK.Ng.Zorro.BackOffice/BackOfficePackage.cs
@@ -5,9 +5,6 @@ using CK.TypeScript;
 namespace CK.Ng.Zorro;
 
 [TypeScriptPackage]
-[TypeScriptImportLibrary( "@angular/cdk", "^19", DependencyKind.Dependency, ForceUse = true )]
-[NgProviderImport( "provideAnimationsAsync", LibraryName = "@angular/platform-browser/animations/async" )]
-[NgProvider( "provideAnimationsAsync()" )]
 [Requires<CK.Ng.Zorro.TSPackage>]
 public class BackOfficePackage : TypeScriptPackage
 {
